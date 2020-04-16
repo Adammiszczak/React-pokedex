17673e73516e44d78847c55b35c9a9132e346f44
Adam Miszczak <adammiszczak1@gmail.com>
2020-04-16 20:03:58 +0200

    Updates

---
+++ precache-manifest.f1ea79269cb2dda35f92cf53c2432d14.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a98ed925b349636ef9c381beaee8d4a2",
+    "url": "/React-pokedex/index.html"
+  },
+  {
+    "revision": "5180ef0b0c7c3639da63",
+    "url": "/React-pokedex/static/css/main.b7b9194a.chunk.css"
+  },
+  {
+    "revision": "4a39324c97c6bd3f0786",
+    "url": "/React-pokedex/static/js/2.527a77ea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-pokedex/static/js/2.527a77ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5180ef0b0c7c3639da63",
+    "url": "/React-pokedex/static/js/main.0d5d491e.chunk.js"
+  },
+  {
+    "revision": "7625b47383d6a195699f",
+    "url": "/React-pokedex/static/js/runtime-main.ddd6b903.js"
+  },
+  {
+    "revision": "9dde281c08b50e2a47bf441ca9baec70",
+    "url": "/React-pokedex/static/media/evoulution-pokemon-40043879-1920-1080.9dde281c.jpg"
+  },
+  {
+    "revision": "ac89d4f9efa30c2a9a4effd2e89f6992",
+    "url": "/React-pokedex/static/media/psyduck.ac89d4f9.png"
+  }
+]);
